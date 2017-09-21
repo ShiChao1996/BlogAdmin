489ec134dbfeb3007ba3d8572c483a2f45697987
ShiChao1996 <2483061998@qq.com>
2017-09-21 15:08:33 +0800

    添加redux 管理状态 #1

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ import {
     hashHistory,
     IndexRoute
 } from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import Login from './src/pages/login';
-import ArticleDetail from './src/pages/articleDetail';
+import ArticleDetail from './src/pages/article/articleDetail';
+import Articles from './src/pages/article/articles';
+import reducers from './src/reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 const router = (
+  <Provider store={store}>
     <Router history={hashHistory}>
-        <Route path="/">
-            <IndexRoute component={Login} />
-            <Route path="/articles" component={ArticleDetail}/>
-        </Route>
-
+      <Route path="/">
+        <IndexRoute component={Login} />
+        <Route path="/articles" component={Articles}/>
+        <Route path="/articledetail" component={ArticleDetail}/>
+      </Route>
     </Router>
+  </Provider>
+
 )
 
 ReactDOM.render(router, document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from "redux";
+import { admin } from './admin';
+
+module.exports = combineReducers( {
+  admin: admin
+});
