1250fc956f857869a843e65c9781e309ddaf4be4
ShiChao1996 <2483061998@qq.com>
2017-09-23 11:08:26 +0800

    添加工具函数 #1

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -8,12 +8,34 @@ function copyAttrExcept(source, ...args) {
   return res;
 }
 
+function copyAttr(target, source, cover) {
+  for (const attr in source) {
+    if (source.hasOwnProperty(attr)) {
+      if (!target.hasOwnProperty(attr)) {
+        target[ attr ] = source[ attr ];
+      } else if (cover) {
+        target[ attr ] = source[ attr ];
+      }
+    }
+  }
+  return target;
+}
+
 function zeroArr(length) {
   return [ ...Array(length) ].map(_ => 0);
 }
 
+function generalKey() {
+  let i = 1;
+  if(i > 1000){
+    i = 1;
+  }
+  return Date.now().toString() + i;
+}
 
 export const tools = {
   copyAttrExcept,
-  zeroArr
+  zeroArr,
+  copyAttr,
+  generalKey
 }
